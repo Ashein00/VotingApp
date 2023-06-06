7a09a83e517ce27a008f4308f2ce5753a96a8e87
VidushaSanidu <vidushsanidu07@gmail.com>
2023-06-06 09:35:31 +0530

    add gender button

--- app.js
+++ app.js
@@ -24,10 +24,6 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
 
-
-
-
-
 //get methods
 
 app.get("/", function (req, res) {
