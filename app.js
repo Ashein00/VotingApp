4a2503808aa7023c7ec22af9346b03d7a67108c8
sanjueranga <sanjueranga16@gmail.com>
2023-06-09 19:49:16 +0530

    cu

--- app.js
+++ app.js
@@ -176,7 +176,7 @@ app.post("/c_register", async function (req, res) {
 });
 
 app.post("/vote", async function (req, res) {
- 
+ console.log(currentUser);
   if (!currentUser.voted) {
     try {
       const vote = req.body.myCheckbox;
