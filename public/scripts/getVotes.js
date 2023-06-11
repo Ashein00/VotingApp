806e0ec85f3b534fc3e4c4bc8e26e32b9266c65f
VidushaSanidu <vidushsanidu07@gmail.com>
2023-06-11 11:10:50 +0530

    bug fixed in results

--- public/scripts/getVotes.js
+++ public/scripts/getVotes.js
@@ -6,6 +6,9 @@ function getVotes(votes) {
     for (const obj of votes) {
         const vote = [obj.vote1,obj.vote2,obj.vote3];
         for (var i=0; i<3;i++){
+            if (vote[i] === ""){
+                continue;
+            }
             if (!perCandiVote[vote[i]]) {
                 perCandiVote[vote[i]] = 0;
             }
